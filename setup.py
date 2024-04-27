52a0df09d826ea41ac52d48b3e78e7926c9255a5
Huy Hoàng Nguyễn <cavangmauxanh@gmail.com>
2024-04-27 21:23:33 +0700

    cavang

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import find_packages, setup
 setup(
     name="segment_anything",
     version="1.0",
-    install_requires=["icecream"],
+    install_requires=[],
     packages=find_packages(exclude="notebooks"),
     extras_require={
         "all": ["matplotlib", "pycocotools", "opencv-python", "onnx", "onnxruntime", "timm"],
