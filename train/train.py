a7e6a97624f716c1583aad3b4f292df0698f04dc
Huy Hoàng Nguyễn <cavangmauxanh@gmail.com>
2024-05-01 20:07:22 +0700

    Update train.py

--- train/train.py
+++ train/train.py
@@ -50,8 +50,8 @@ class mobilenetv3Large(nn.Module):
 class CrossBranchAdapter(nn.Module):
     def __init__(self):
         super(CrossBranchAdapter, self).__init__()
-        self.conv = nn.Conv2d(in_channels=256,out_channels=256,kernel_size=3, padding=1, stride=1)
-        self.upchannel=nn.Conv2d(in_channels=256,out_channels=768,kernel_size=1,stride=1)
+        self.conv = nn.Conv2d(in_channels=256,out_channels=128,kernel_size=3, padding=1, stride=1)
+        self.upchannel=nn.Conv2d(in_channels=128,out_channels=768,kernel_size=1,stride=1)
         self.downchannel=nn.Conv2d(in_channels=1536,out_channels=256,kernel_size=1,stride=1)
         self.max_pool = nn.MaxPool2d(kernel_size=3, stride=1,padding=1)
         self.mean_pool = nn.AvgPool2d(kernel_size=3, stride=1,padding=1)
