92e260a659bc2e4699ca3d7952f4aeafba812736
Huy Hoàng Nguyễn <cavangmauxanh@gmail.com>
2024-05-04 20:37:02 +0700

    Update train.py

--- train/train.py
+++ train/train.py
@@ -825,8 +825,8 @@ if __name__ == "__main__":
 
     # valid set
     dataset_coift_val = {"name": "COIFT",
-                 "im_dir": "./data/thin_object_detection/COIFT/images",
-                 "gt_dir": "./data/thin_object_detection/COIFT/masks",
+                 "im_dir": "/kaggle/input/thinobject5k/thin_object_detection/COIFT/images",
+                 "gt_dir": "/kaggle/input/thinobject5k/thin_object_detection/COIFT/masks",
                  "im_ext": ".jpg",
                  "gt_ext": ".png"}
 
@@ -849,7 +849,7 @@ if __name__ == "__main__":
                  "gt_ext": ".png"}
 
     train_datasets = [dataset_thin_val]
-    valid_datasets = [dataset_hrsod_val] 
+    valid_datasets = [dataset_coift_val] 
 
     # args = get_args_parser()
     net = MaskDecoderHQ("vit_b") 
