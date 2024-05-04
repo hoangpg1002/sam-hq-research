ee976672cf925e1f0b1d8dff18c488c123f86031
Huy Hoàng Nguyễn <cavangmauxanh@gmail.com>
2024-05-04 09:53:30 +0700

    Update image_encoder.py

--- train/segment_anything_training/modeling/image_encoder.py
+++ train/segment_anything_training/modeling/image_encoder.py
@@ -213,8 +213,7 @@ class Block(nn.Module):
             x = window_unpartition(x, self.window_size, pad_hw, (H, W))
 
         x = shortcut + x
-        feature = self.cross_branch_adapter(self.norm2(x),add_features)
-        x = x + self.mlp(self.norm2(x)) + feature
+        x = x + self.mlp(self.norm2(x)) 
 
         return x
 
