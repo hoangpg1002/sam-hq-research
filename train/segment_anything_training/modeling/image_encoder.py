baef41acd3efce9fc9253461360139c471afd64b
Huy Hoàng Nguyễn <cavangmauxanh@gmail.com>
2024-05-05 19:03:02 +0700

    Update image_encoder.py

--- train/segment_anything_training/modeling/image_encoder.py
+++ train/segment_anything_training/modeling/image_encoder.py
@@ -14,9 +14,9 @@ from .common import LayerNorm2d, MLPBlock
 class CrossBranchAdapter(nn.Module):
     def __init__(self):
         super(CrossBranchAdapter, self).__init__()
-        self.conv = nn.Sequential(nn.Conv2d(in_channels=512,out_channels=512,kernel_size=7, padding=3, stride=1,groups=512),nn.Sigmoid(),nn.Dropout(0.1))
-        self.upchannel=nn.Conv2d(in_channels=512,out_channels=768,kernel_size=1,stride=1)
-        self.downchannel=nn.Conv2d(in_channels=768,out_channels=256,kernel_size=1,stride=1)
+        self.conv = nn.Sequential(nn.Conv2d(in_channels=768,out_channels=768,kernel_size=7, padding=3, stride=1,groups=768),nn.Sigmoid(),nn.Dropout(0.1))
+        #self.upchannel=nn.Conv2d(in_channels=512,out_channels=768,kernel_size=1,stride=1)
+        self.downchannel=nn.Conv2d(in_channels=768,out_channels=384,kernel_size=1,stride=1)
         self.max_pool = nn.MaxPool2d(kernel_size=2,padding=0,stride=2)
         self.mean_pool = nn.AvgPool2d(kernel_size=2,padding=0,stride=2)
         self.HW=nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
@@ -38,7 +38,7 @@ class CrossBranchAdapter(nn.Module):
         conv_out=self.conv(pooled_concat)
         conv_out=self.HW(conv_out)
         # Convolutional layer
-        conv_out = self.upchannel(conv_out) * shortcut + shortcut #torch.Size([1, 768, 64, 64])
+        conv_out = conv_out * shortcut + shortcut #torch.Size([1, 768, 64, 64])
         #print(conv_out.shape) #torch.Size([1, 768, 64, 64])
         # batch_size, num_channels,height,width = conv_out.size()
         # conv_out = conv_out.contiguous().view(batch_size,num_channels, height * width)
