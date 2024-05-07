333b7dfcfcdd66e6e5b1ad7a1d038add17e65f48
Huy Hoàng Nguyễn <cavangmauxanh@gmail.com>
2024-05-07 18:47:22 +0700

    Update image_encoder.py

--- train/segment_anything_training/modeling/image_encoder.py
+++ train/segment_anything_training/modeling/image_encoder.py
@@ -20,7 +20,7 @@ class CrossBranchAdapter(nn.Module):
         self.max_pool = nn.MaxPool2d(kernel_size=2,padding=0,stride=2)
         self.mean_pool = nn.AvgPool2d(kernel_size=2,padding=0,stride=2)
         self.HW=nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-        self.mlp = MLPBlock(embedding_dim=768, mlp_dim=int(768 * 2), act=nn.GELU)
+        #self.mlp = MLPBlock(embedding_dim=768, mlp_dim=int(768 * 2), act=nn.GELU)
     def forward(self, tensor1, tensor2):
         # Concatenate 2 tensors along the channel dimension
         concat_tensor = tensor1.permute(0, 3, 1, 2) + tensor2.permute(0, 3, 1, 2) #([1, 768, 64, 64])
@@ -39,8 +39,8 @@ class CrossBranchAdapter(nn.Module):
         # Convolutional layer
         conv_out = conv_out * shortcut + shortcut #torch.Size([1, 768, 64, 64])
         #print(conv_out.shape) #torch.Size([1, 768, 64, 64])
-        conv_out=self.mlp(conv_out.permute(0,2,3,1)) 
-        return conv_out + shortcut.permute(0,2,3,1)
+        #conv_out=self.mlp(conv_out.permute(0,2,3,1)) 
+        return conv_out 
 # This class and its supporting functions below lightly adapted from the ViTDet backbone available at: https://github.com/facebookresearch/detectron2/blob/main/detectron2/modeling/backbone/vit.py # noqa
 class ImageEncoderViT(nn.Module):
     def __init__(
