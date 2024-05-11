2a7bd099bab844fb1aa98292f7b9c55ab9e56117
Huy Hoàng Nguyễn <cavangmauxanh@gmail.com>
2024-05-11 08:58:13 +0700

    Update train.py

--- train/train.py
+++ train/train.py
@@ -883,8 +883,8 @@ if __name__ == "__main__":
                  "gt_ext": ".png"}
 
     dataset_thin = {"name": "ThinObject5k-TR",
-                 "im_dir": "/kaggle/input/thinobject5k/thin_object_detection/ThinObject5K/images_train",
-                 "gt_dir": "/kaggle/input/thinobject5k/thin_object_detection/ThinObject5K/masks_train",
+                 "im_dir": "/kaggle/input/hq44kseg/thin_object_detection/ThinObject5K/images_train",
+                 "gt_dir": "/kaggle/input/hq44kseg/thin_object_detection/ThinObject5K/masks_train",
                  "im_ext": ".jpg",
                  "gt_ext": ".png"}
 
@@ -920,8 +920,8 @@ if __name__ == "__main__":
 
     # valid set
     dataset_coift_val = {"name": "COIFT",
-                 "im_dir": "/kaggle/input/thinobject5k/thin_object_detection/COIFT/images",
-                 "gt_dir": "/kaggle/input/thinobject5k/thin_object_detection/COIFT/masks",
+                 "im_dir": "/kaggle/input/hq44kseg/thin_object_detection/COIFT/images",
+                 "gt_dir": "/kaggle/input/hq44kseg/thin_object_detection/COIFT/masks",
                  "im_ext": ".jpg",
                  "gt_ext": ".png"}
 
@@ -932,8 +932,8 @@ if __name__ == "__main__":
                  "gt_ext": ".png"}
 
     dataset_thin_val = {"name": "ThinObject5k-TE",
-                 "im_dir": "/kaggle/input/thinobject5k/thin_object_detection/ThinObject5K/images_test",
-                 "gt_dir": "/kaggle/input/thinobject5k/thin_object_detection/ThinObject5K/masks_test",
+                 "im_dir": "/kaggle/input/hq44kseg/thin_object_detection/ThinObject5K/images_test",
+                 "gt_dir": "/kaggle/input/hq44kseg/thin_object_detection/ThinObject5K/masks_test",
                  "im_ext": ".jpg",
                  "gt_ext": ".png"}
 
@@ -943,10 +943,10 @@ if __name__ == "__main__":
                  "im_ext": ".jpg",
                  "gt_ext": ".png"}
 
-    #train_datasets = [dataset_dis, dataset_thin, dataset_fss, dataset_duts, dataset_duts_te, dataset_ecssd, dataset_msra]
-    train_datasets = [dataset_thin]
-    #valid_datasets = [dataset_dis_val, dataset_coift_val, dataset_hrsod_val, dataset_thin_val] 
-    valid_datasets = [dataset_thin_val,dataset_coift_val,dataset_hrsod_val] 
+    train_datasets = [dataset_dis, dataset_thin, dataset_fss, dataset_duts, dataset_duts_te, dataset_ecssd, dataset_msra]
+    #train_datasets = [dataset_thin]
+    valid_datasets = [dataset_dis_val, dataset_coift_val, dataset_hrsod_val, dataset_thin_val] 
+    #valid_datasets = [dataset_thin_val,dataset_coift_val,dataset_hrsod_val] 
 
     # args = get_args_parser()
     net = MaskDecoderHQ("vit_b") 
